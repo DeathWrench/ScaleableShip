baf1c4fd138821c03d25a8597a6ec6064823555e
DeathWrench <45341450+DeathWrench@users.noreply.github.com>
2024-02-18 03:43:12 -0500

    Add files via upload

--- Config.cs
+++ Config.cs
@@ -12,9 +12,31 @@ namespace ScaleableShip
         }
 
         public static ConfigEntry<bool> activateMode { get; private set; }
+        public static ConfigEntry<bool> newMode { get; private set; }
         public static ConfigEntry<float> shipScaleX { get; private set; }
         public static ConfigEntry<float> shipScaleY { get; private set; }
         public static ConfigEntry<float> shipScaleZ { get; private set; }
+        public static ConfigEntry<float> shipPositionX { get; private set; }
+        public static ConfigEntry<float> shipPositionY { get; private set; }
+        public static ConfigEntry<float> shipPositionZ { get; private set; }
+        public static ConfigEntry<float> shipRailScaleX { get; private set; }
+        public static ConfigEntry<float> shipRailScaleY { get; private set; }
+        public static ConfigEntry<float> shipRailScaleZ { get; private set; }
+        public static ConfigEntry<float> shipRailPositionX { get; private set; }
+        public static ConfigEntry<float> shipRailPositionY { get; private set; }
+        public static ConfigEntry<float> shipRailPositionZ { get; private set; }
+        public static ConfigEntry<float> shipRailRotationX { get; private set; }
+        public static ConfigEntry<float> shipRailRotationY { get; private set; }
+        public static ConfigEntry<float> shipRailRotationZ { get; private set; }
+        public static ConfigEntry<float> shipRailPostScaleX { get; private set; }
+        public static ConfigEntry<float> shipRailPostScaleY { get; private set; }
+        public static ConfigEntry<float> shipRailPostScaleZ { get; private set; }
+        public static ConfigEntry<float> shipRailPostPositionX { get; private set; }
+        public static ConfigEntry<float> shipRailPostPositionY { get; private set; }
+        public static ConfigEntry<float> shipRailPostPositionZ { get; private set; }
+        public static ConfigEntry<float> shipRailPostRotationX { get; private set; }
+        public static ConfigEntry<float> shipRailPostRotationY { get; private set; }
+        public static ConfigEntry<float> shipRailPostRotationZ { get; private set; }
         public static ConfigEntry<float> catwalkshipScaleX { get; private set; }
         public static ConfigEntry<float> catwalkshipScaleY { get; private set; }
         public static ConfigEntry<float> catwalkshipScaleZ { get; private set; }
@@ -28,9 +50,34 @@ namespace ScaleableShip
         {
             configFile = cfg;
             activateMode = cfg.Bind("Options", "Enabled", true, "Enables the mod.");
+            newMode = cfg.Bind("Options", "New Mode", true, "Enables the new mode which scales everything, only uses Ship Scale values.");
             shipScaleX = cfg.Bind("Options", "Ship Scale X", 2f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
             shipScaleY = cfg.Bind("Options", "Ship Scale Y", 2f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
             shipScaleZ = cfg.Bind("Options", "Ship Scale Z", 1f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
+            shipPositionX = cfg.Bind("Options", "Ship Position X", 0f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
+            shipPositionY = cfg.Bind("Options", "Ship Position Y", 0f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
+            shipPositionZ = cfg.Bind("Options", "Ship Position Z", 0f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
+            shipRailScaleX = cfg.Bind("Options", "Ship Rail Scale X", 0f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
+            shipRailScaleY = cfg.Bind("Options", "Ship Rail Scale Y", 0f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
+            shipRailScaleZ = cfg.Bind("Options", "Ship Rail Scale Z", 0f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPositionX = cfg.Bind("Options", "Ship Rail Position X", 0f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPositionY = cfg.Bind("Options", "Ship Rail Position Y", 0f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPositionZ = cfg.Bind("Options", "Ship Rail Position Z", 0f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
+            shipRailRotationX = cfg.Bind("Options", "Ship Rail Rotation X", 0f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
+            shipRailRotationY = cfg.Bind("Options", "Ship Rail Rotation Y", 0f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
+            shipRailRotationZ = cfg.Bind("Options", "Ship Rail Rotation Z", 0f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPostScaleX = cfg.Bind("Options", "Ship Rail Post Scale X", 0f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPostScaleY = cfg.Bind("Options", "Ship Rail Post Scale Y", 0f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPostScaleZ = cfg.Bind("Options", "Ship Rail Post Scale Z", 0f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPostPositionX = cfg.Bind("Options", "Ship Rail Post Position X", 0f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPostPositionY = cfg.Bind("Options", "Ship Rail Post Position Y", 0f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPostPositionZ = cfg.Bind("Options", "Ship Rail Post Position Z", 0f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPostRotationX = cfg.Bind("Options", "Ship Rail Post Rotation X", 0f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPostRotationY = cfg.Bind("Options", "Ship Rail Post Rotation Y", 0f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
+            shipRailPostRotationZ = cfg.Bind("Options", "Ship Rail Post Rotation Z", 0f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
+            shipPositionX = cfg.Bind("Options", "Ship Position X", 0f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
+            shipPositionY = cfg.Bind("Options", "Ship Position Y", 0f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
+            shipPositionZ = cfg.Bind("Options", "Ship Position Z", 0f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
             catwalkshipScaleX = cfg.Bind("Options", "Catwalk Scale X", 1.9f, "X Coordinates\n Left/Right");// Make sure you put a ''f'' at the end of the number.");
             catwalkshipScaleY = cfg.Bind("Options", "Catwalk Scale Y", 2f, "Y Coordinates\n Up/Down");// Make sure you put a ''f'' at the end of the number.");
             catwalkshipScaleZ = cfg.Bind("Options", "Catwalk Scale Z", 0.2f, "Z Coordinates\n Front/Back");// Make sure you put a ''f'' at the end of the number.");
--- Plugin.cs
+++ Plugin.cs
@@ -17,48 +17,86 @@ namespace ScaleableShip
             Logger.LogInfo($"Plugin {"ScaleableTelevision"} is loaded!");
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
         }
-    
-
         private void Update()
         {
             if (ConfigManager.activateMode.Value)
             {
-                //GameObject cozylights = GameObject.Find("CozyLights");
-                //GameObject cozylightslod1 = GameObject.Find("CozyLightsLOD1");
-                GameObject shipinside = GameObject.Find("ShipInside");
-                GameObject shipinside001 = GameObject.Find("ShipInside.001");
-                GameObject shiphull = GameObject.Find("ShipHull");
-                //GameObject shiprails = GameObject.Find("ShipRails");
-                //GameObject shiprailposts = GameObject.Find("ShipRailPosts");
-                //GameObject shipsupportbeams = GameObject.Find("ShipSupportBeams");
-                //GameObject spawnroom = GameObject.Find("SpawnRoom");
-                //GameObject spawnroom0 = GameObject.Find("SpawnRoom_0");
-                //GameObject shipsupportbeams001 = GameObject.Find("ShipSupportBeams.001");
-                //GameObject catwalkraillining = GameObject.Find("CatwalkRailLining");
-                //GameObject catwalkrailliningb = GameObject.Find("CatwalkRailLiningB");
-                //GameObject catwalkrailship = GameObject.Find("CatwalkRailShip");
-                //GameObject catwalkrailpost = GameObject.Find("CatwalkRailPost");
-                GameObject catwalkship = GameObject.Find("CatwalkShip");
-                //GameObject pbmesh39048 = GameObject.Find("pb_Mesh39048");
-                //GameObject pbmesh84786 = GameObject.Find("pb_Mesh84786");
-                //spawnroom.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //spawnroom0.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                shipinside.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                shipinside001.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                shiphull.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //shiprails.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //shiprailposts.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //shipsupportbeams.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //shipsupportbeams001.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //catwalkraillining.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //catwalkrailliningb.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //catwalkrailship.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //catwalkrailpost.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                catwalkship.transform.localScale = new Vector3(ConfigManager.catwalkshipScaleX.Value, ConfigManager.catwalkshipScaleY.Value, ConfigManager.catwalkshipScaleZ.Value);
-                catwalkship.transform.localPosition = new Vector3(ConfigManager.catwalkshipPositionX.Value, ConfigManager.catwalkshipPositionY.Value, ConfigManager.catwalkshipPositionZ.Value);
-                //pbmesh39048.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
-                //pbmesh84786.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                if (ConfigManager.newMode.Value)
+                {
+                    StartOfRound.Instance.elevatorTransform.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                }
+                else
+                {
+                    //GameObject cozylights = GameObject.Find("CozyLights");
+                    //GameObject cozylightslod1 = GameObject.Find("CozyLightsLOD1");
+                    GameObject shipinside = GameObject.Find("ShipInside");
+                    GameObject shipinside001 = GameObject.Find("ShipInside.001");
+                    GameObject shiphull = GameObject.Find("ShipHull");
+                    GameObject shiprails = GameObject.Find("ShipRails");
+                    GameObject shiprailposts = GameObject.Find("ShipRailPosts");
+                    //GameObject shipsupportbeams = GameObject.Find("ShipSupportBeams");
+                    //GameObject spawnroom = GameObject.Find("SpawnRoom");
+                    //GameObject spawnroom0 = GameObject.Find("SpawnRoom_0");
+                    //GameObject shipsupportbeams001 = GameObject.Find("ShipSupportBeams.001");
+                    //GameObject catwalkraillining = GameObject.Find("CatwalkRailLining");
+                    //GameObject catwalkrailliningb = GameObject.Find("CatwalkRailLiningB");
+                    //GameObject catwalkrailship = GameObject.Find("CatwalkRailShip");
+                    //GameObject catwalkrailpost = GameObject.Find("CatwalkRailPost");
+                    GameObject catwalkship = GameObject.Find("CatwalkShip");
+                    //GameObject pbmesh39048 = GameObject.Find("pb_Mesh39048");
+                    //GameObject pbmesh84786 = GameObject.Find("pb_Mesh84786");
+                    //spawnroom.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    //spawnroom0.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    shipinside.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    shipinside001.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    shiphull.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    shipinside.transform.localPosition = new Vector3(ConfigManager.shipPositionX.Value, ConfigManager.shipPositionY.Value, ConfigManager.shipPositionZ.Value);
+                    shipinside001.transform.localPosition = new Vector3(ConfigManager.shipPositionX.Value, ConfigManager.shipPositionY.Value, ConfigManager.shipPositionZ.Value);
+                    shiphull.transform.localPosition = new Vector3(ConfigManager.shipPositionX.Value, ConfigManager.shipPositionY.Value, ConfigManager.shipPositionZ.Value);
+                    shiprails.transform.localScale = new Vector3(ConfigManager.shipRailScaleX.Value, ConfigManager.shipRailScaleY.Value, ConfigManager.shipRailScaleZ.Value);
+                    shiprails.transform.Rotate(ConfigManager.shipRailRotationX.Value, ConfigManager.shipRailRotationY.Value, ConfigManager.shipRailRotationZ.Value, Space.World);
+                    shiprails.transform.localPosition = new Vector3(ConfigManager.shipRailPositionX.Value, ConfigManager.shipRailPositionX.Value, ConfigManager.shipRailPositionX.Value);
+                    shiprailposts.transform.localScale = new Vector3(ConfigManager.shipRailPostScaleX.Value, ConfigManager.shipRailPostScaleY.Value, ConfigManager.shipRailPostScaleZ.Value);
+                    shiprailposts.transform.localPosition = new Vector3(ConfigManager.shipRailPostPositionX.Value, ConfigManager.shipRailPostPositionY.Value, ConfigManager.shipRailPostPositionZ.Value);
+                    shiprailposts.transform.Rotate(ConfigManager.shipRailPostRotationX.Value, ConfigManager.shipRailPostRotationY.Value, ConfigManager.shipRailPostRotationZ.Value, Space.World);
+                    //shipsupportbeams.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    //shipsupportbeams001.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    //catwalkraillining.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    //catwalkrailliningb.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    //catwalkrailship.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    //catwalkrailpost.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    catwalkship.transform.localScale = new Vector3(ConfigManager.catwalkshipScaleX.Value, ConfigManager.catwalkshipScaleY.Value, ConfigManager.catwalkshipScaleZ.Value);
+                    catwalkship.transform.localPosition = new Vector3(ConfigManager.catwalkshipPositionX.Value, ConfigManager.catwalkshipPositionY.Value, ConfigManager.catwalkshipPositionZ.Value);
+                    //pbmesh39048.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                    //pbmesh84786.transform.localScale = new Vector3(ConfigManager.shipScaleX.Value, ConfigManager.shipScaleY.Value, ConfigManager.shipScaleZ.Value);
+                }
             }
         }
+        // Token: 0x06000021 RID: 33 RVA: 0x000028F0 File Offset: 0x00000AF0
+        // Token: 0x06000022 RID: 34 RVA: 0x00002906 File Offset: 0x00000B06
+        protected internal string __getTypeName()
+        {
+            return "AutoParentToShip";
+        }
+
+        // Token: 0x0400002B RID: 43
+        public bool disableObject;
+
+        // Token: 0x0400002C RID: 44
+        public Vector3 positionOffset;
+
+        // Token: 0x0400002D RID: 45
+        public Vector3 rotationOffset;
+
+        // Token: 0x0400002E RID: 46
+        [HideInInspector]
+        public Vector3 startingPosition;
+
+        // Token: 0x0400002F RID: 47
+        [HideInInspector]
+        public Vector3 startingRotation;
+
+        // Token: 0x04000030 RID: 48
+        public bool overrideOffset;
     }
 }
